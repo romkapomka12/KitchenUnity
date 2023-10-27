7126da4f2189290d34587d03c485401846d2f194
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-10-27 10:11:14 +0300

    SECRET_KEY = os.environ.get(DJANGO_SECRET_KEY)

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
