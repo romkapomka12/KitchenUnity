135e8f3884e5a7e87b3cb5bf0de0c10b3d3df49f
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-10-31 13:18:47 +0200

    database connection PostgreSQL

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
 
     }
 }
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES["default"].update(dict(db_from_env))
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(dict(db_from_env))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
